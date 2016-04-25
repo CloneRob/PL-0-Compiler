1a7abdb1a82fbd3751bf62b4cb49b17fabd4bf21
CloneRob <ropamen@gmail.com>
2016-04-25 09:30:58 +0200

    Added basic version of iterator for lexer struct

--- src/lexer/mod.rs
+++ src/lexer/mod.rs
@@ -1,3 +1,5 @@
+mod token;
+
 pub struct Lexer {
     buf: Vec<char>,
     pos: usize,
@@ -10,6 +12,33 @@ impl Lexer {
             buf: src.chars().collect(),
             pos: 0,
             last_symbol: None,
+        }
+    }
+
+    fn is_alphabetical(&mut self) -> token::Token{
+        token::Token::Add
+    }
+
+    fn is_numerical(&mut self) -> token::Token {
+        token::Token::Sub
+    }
+}
+
+impl Iterator for Lexer {
+    type Item = token::Token;
+    fn next(&mut self) -> Option<token::Token> {
+        match self.buf[self.pos] {
+            '+' => Some(token::Token::Add),
+            '-' => Some(token::Token::Sub),
+            '*' => Some(token::Token::Mul),
+            '/' => Some(token::Token::Div),
+
+            '(' => Some(token::Token::LParen),
+            ')' => Some(token::Token::RParen),
+
+            '<' => Some(token::Token::Ls),
+            '>' => Some(token::Token::Gr),
+            _ => None
         }
     }
 }
---
+++ src/lexer/token.rs
@@ -0,0 +1,14 @@
+pub enum Token {
+    Add,
+    Sub,
+    Mul,
+    Div,
+
+    LParen,
+    RParen,
+
+    Gr,
+    Ge,
+    Ls,
+    Le,
+}
